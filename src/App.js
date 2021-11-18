11508ec4b930ebf5e3a068d9dc6b6f004ac1d461
husan <test>
2021-11-18 20:31:37 +0500

    dark mode done using ThemeProvider styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import { useDarkMode } from './useDarkMode';
+import Toggle from './toggle';
+import styles from './App.css';
+
+console.log(styles);
+
 
 function App() {
+  const [theme, toggleTheme] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+  // The function that toggles between themes
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+
+      <GlobalStyles />
+      <Toggle theme={theme} toggleTheme={toggleTheme} />
+      <h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
+      <footer>
+        <span>Credits:</span>
+        <small><b>Sun</b> icon made by <a href="https://www.flaticon.com/authors/smalllikeart">smalllikeart</a> from <a href="https://www.flaticon.com">www.flaticon.com</a></small>
+        <small><b>Moon</b> icon made by <a href="https://www.freepik.com/home">Freepik</a> from <a href="https://www.flaticon.com">www.flaticon.com</a></small>
+      </footer>
+    </ThemeProvider>
+  )
 }
 
 export default App;
